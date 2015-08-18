1f8fadbe0be0e761bdbe0356ca85c034a8a31cc1
atypical <amador.e.rivera@gmail.com>
2015-08-18 16:51:42 -0700

    Initial Commit. Includes the Driver class and AdminControlPanel which displays an empty window.

---
+++ src/AdminControlPanel.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class AdminControlPanel extends JFrame {
+	public AdminControlPanel() {
+		setTitle("Mini Twitter");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(700, 500);
+		
+		//Initializers
+		
+		
+		//pack();
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	
+	
+}
---
+++ src/Driver.java
@@ -0,0 +1,12 @@
+import javax.swing.SwingUtilities;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(() -> {
+			new AdminControlPanel();
+		});
+	}
+
+}
