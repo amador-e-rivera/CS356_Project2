6bbf686d4152efb4bc17233b1238aee051963b1b
atypical <amador.e.rivera@gmail.com>
2015-08-22 01:09:08 -0700

    Added recursive find to UserGroup

--- src/Driver.java
+++ src/Driver.java
@@ -3,9 +3,29 @@ import javax.swing.SwingUtilities;
 public class Driver {
 
 	public static void main(String[] args) {
+		UserGroup group1 = new UserGroup("root");
+		UserGroup group2 = new UserGroup("CS356");
+		UserGroup group3 = new UserGroup("CS408");
+		
 		User user1 = new User("Amador");
 		User user2 = new User("Linh");
 		User user3 = new User("Giang");
+		User user4 = new User("Amber");
+		
+		group1.addUser(user1);
+		group1.addUser(user2);
+		group2.addUser(user3);
+		group3.addUser(user4);
+		
+		group1.addUser(group2);
+		group2.addUser(group3);
+
+		if (group1.findUser("Amber", group1)) {
+			System.out.println("User Exists");
+		} else {
+			System.out.println("User Not Found");
+		}
+		
 		
 		user1.addFollower(user2);
 		user1.addFollower(user3);
--- src/UserGroup.java
+++ src/UserGroup.java
@@ -1,26 +1,46 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.tree.DefaultMutableTreeNode;
+
 public class UserGroup extends User {
 
 	Map<String, User> users;
-	
+
 	public UserGroup(String id) {
 		super(id);
 		users = new HashMap<String, User>();
 	}
-	
+
 	public void addUser(User user) {
 		String id = user.getUserId();
-		
-		if(!users.containsKey(id)) {
+
+		if (!users.containsKey(id)) {
 			users.put(id, user);
 		} else {
 			System.out.println("User id " + id + " already exists!");
 		}
 	}
-	
+
 	public Map<String, User> getUserGroups() {
 		return users;
+	}
+
+	public boolean findUser(String id, UserGroup userGroup) {
+		if (userGroup.getUserId().equals(id)) {
+			return true;
+		}
+
+		if (!userGroup.getUserGroups().isEmpty()) {
+			for (Map.Entry<String, User> group : userGroup.getUserGroups().entrySet()) {
+				if (group.getValue() instanceof UserGroup && findUser(id, (UserGroup) group.getValue())) {
+					return true;
+				} else if (group.getKey().equals(id)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 }
